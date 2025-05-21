945bf8d1c187b56657767c74052ca71948b32158
Wang Hao <991060@gmail.com>
2025-05-21 18:25:20 +0800

    Clean up the UI.

--- src/App.vue
+++ src/App.vue
@@ -13,16 +13,6 @@ const handleFolderSelected = (path: string) => {
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  
   <div class="container">
     <h2>Folder Selection</h2>
     <FolderSelector 
