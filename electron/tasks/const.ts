d03451d30d658967485884f07c2c4c2fa78ccdc0
Wang Hao <991060@gmail.com>
2025-05-22 08:27:01 +0800

    Delete previous setup.

--- electron/tasks/const.ts
+++
@@ -1,8 +0,0 @@
-// MUST be kept in sync with https://github.com/directivegames/genesys.ai/blob/develop/src/const.ts
-export const DEFAULT_SCENE_NAME = 'default.scene.json';
-export const PUBLIC_FOLDER: string = 'public';
-export const PROJECT_PREFIX: string = '@project';
-export const ENGINE_PREFIX: string = '@engine';
-export const DEFAULT_GAME_NAME: string = 'game.ts';
-export const DEFAULT_GAME_BUNDLE_NAME: string = 'game.js';
-export const BUILT_PROJECT_FOLDER: string = '.dist';
