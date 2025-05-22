640ba6c87feec4ab554846a4fcc4f4e6fe99939c
Wang Hao <991060@gmail.com>
2025-05-22 11:45:47 +0800

    Move code to sub folders.

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -20,7 +20,7 @@ const createWindow = async () => {
     await win.loadURL('http://localhost:5173');
     win.webContents.openDevTools();
   } else {
-    await win.loadFile(path.join(__dirname, '../dist/index.html'));    
+    await win.loadFile(path.join(__dirname, '../index.html'));    
   }
 };
 
